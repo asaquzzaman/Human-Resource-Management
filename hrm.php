e83c9dcec4ac7f687b4b4a577a9e42fe4efa1249
asaquzzaman <joy.mishu@gmail.com>
2014-07-28 16:29:38 +0600

    gpl2 licence add

--- hrm.php
+++ hrm.php
@@ -10,6 +10,32 @@
  * TextDomain: hrm
  */
 
+/**
+ * Copyright (c) 2013 Asaquzzaman Mishu (email: joy.mishu@gmail.com). All rights reserved.
+ *
+ * Released under the GPL license
+ * http://www.opensource.org/licenses/gpl-license.php
+ *
+ * This is an add-on for WordPress
+ * http://wordpress.org/
+ *
+ * **********************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ * **********************************************************************
+ */
+
 function hrm_autoload( $class ) {
     $name = explode( '_', $class );
 
